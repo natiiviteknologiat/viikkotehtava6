0a941315adaecd4361e9cc02ec4415045a82135f
Rapuska <118633578+Rapuska@users.noreply.github.com>
2024-10-12 16:10:41 +0300

    scaffold exercise done

--- app/src/main/java/com/example/viikkotehtava6/MainActivity.kt
+++ app/src/main/java/com/example/viikkotehtava6/MainActivity.kt
@@ -1,40 +1,89 @@
 package com.example.viikkotehtava6
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.viikkotehtava6.ui.theme.Viikkotehtava6Theme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             Viikkotehtava6Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ScaffoldApp()
             }
         }
     }
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun ScaffoldApp() {
+    Scaffold(
+        topBar = {
+            MyTopBar()
+        },
+        content = { paddingValues ->
+            Text(
+                text = "Content for Home screen",
+                modifier = Modifier.padding(paddingValues)
+            )
+        }
+        // BottomBar is commented out since we do not want to display it anymore
+        // bottomBar = {
+        //     BottomAppBar {
+        //         Text(text = "Bottom bar")
+        //     }
+        // }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyTopBar() {
+    var expanded by remember { mutableStateOf(false) }
+    TopAppBar(
+        title = {
+            Text(text = "My App")
+        },
+        navigationIcon = {
+            IconButton(onClick = { /* TODO */ }) {
+                Icon(imageVector = Icons.Filled.Menu, contentDescription = null)
+            }
+        },
+        actions = {
+            IconButton(onClick = {
+                expanded = !expanded
+            }) {
+                Icon(imageVector = Icons.Filled.MoreVert, contentDescription = null)
+            }
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false }) {
+                DropdownMenuItem(
+                    text = { Text("Info") },
+                    onClick = { /* TODO */ }
+                )
+                DropdownMenuItem(
+                    text = { Text("Settings") },
+                    onClick = { /* TODO */ }
+                )
+            }
+        }
     )
 }
 
@@ -42,6 +91,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     Viikkotehtava6Theme {
-        Greeting("Android")
+        ScaffoldApp()
     }
 }
